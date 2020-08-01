1a2a67ddf13a5b1a7d2a78ffb24d241a9cac7260
shawn2james <shawn2james@gmail.com>
2020-08-01 18:17:42 +0530

    changed some style of opening screen

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+# imports
 import cv2
 from random import randint
 import PIL.Image as Image
@@ -14,7 +15,6 @@ img_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-
 model = load('final-model.pt', map_location=device('cpu'))
 model.eval()
 
@@ -29,43 +29,51 @@ comp_images = load_comp_images(
     '../comp-images', (COMP_X1, COMP_Y1, COMP_X2, COMP_Y2),
     (X_OFFSET, Y_OFFSET), "png")
 
+# video object to capture image using webcam
 video = cv2.VideoCapture(0)
 
+# the time difference used to predict the hand gesture
 global_previous_time = time()
 global_delta = 0
 
+# the time difference used to determine whether to show play result or not
 play_previous = time()
 play_delta = 0
 
-prev_pred_cls = "none"
+# booleans used to decide what to display
 show_comp_img = False
 make_prediction = True
 display_play_result = False
 
-comp_choices = ['rock', 'paper', 'scissors']
-player_score, comp_score = 0, 0
+#########################################################################################################################
 play = True
+# the first opening screen
 while True:
     ret, frame = video.read()  # returns ret and the frame
     frame = cv2.flip(frame, 1)
 
-    cv2.putText(frame, "Press 'p' to continue and 'q' to quit!", org=(100, 400),
-                fontFace=cv2.FONT_HERSHEY_COMPLEX,
-                fontScale=0.8, color=(255, 255, 255), thickness=2)
+    cv2.putText(frame, "PRESS 'p' TO CONTINUE AND 'q' TO QUIT!", org=(20, 60), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontSca1e=0.8, color=(0, 0, 0), thickness=2)
     cv2.imshow('Image', frame)
     key = cv2.waitKey(1)
+    # quit if 'q' is pressed
     if key == ord('q'):
         play = False
         break
+    # play if 'p' is pressed
     if key == ord('p'):
         break
+##########################################################################################################################
+comp_choices = ['rock', 'paper', 'scissors']
+player_score, comp_score = 0, 0
 
 if play:
     while True:
+        # update global time
         global_current = time()
         global_delta += global_current-global_previous_time
         global_previous_time = global_current
 
+        # update play time
         play_current = time()
         play_delta += play_current-play_previous
         play_previous = play_current
@@ -88,25 +96,28 @@ if play:
             thickness=BORDER_THICKNESS
         )
 
+        # make prediction in 1 second interval
         if global_delta > 1:
             if make_prediction:
                 # MODEL PREDICTION
-                # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
                 img = Image.fromarray(frame)
+                # crop out player's box
                 cropped = img.crop(
                     (PLAYER_X1, PLAYER_Y1, PLAYER_X2, PLAYER_Y2))
                 img = img_transform(cropped).view(1, 3, 224, 224)
+                # convert into correct input format to be passed into the model
                 probs = model(img)
                 pred = argmax(probs).item()
                 pred_cls = "rock" if pred == 0 else "paper" if pred == 1 else "scissors" if pred == 2 else "none"
-                comp_choice = comp_choices[randint(0, 2)]
-                comp_image = comp_images[comp_choices.index(comp_choice)]
+                # if the player is showing a hand gesture
                 if pred_cls != "none":
+                    # choosing a random image as computer's choice
                     comp_choice = comp_choices[randint(0, 2)]
-                    comp_img = comp_images[comp_choices.index(comp_choice)]
+                    comp_image = comp_images[comp_choices.index(comp_choice)]
+
                     play_result = result(pred_cls, comp_choice)
                     show_comp_img = True
+                    # update player score
                     if play_result == 1:
                         player_score += 1
                     elif play_result == -1:
@@ -119,13 +130,16 @@ if play:
 
             global_delta = 0
 
+        # hide computer's image one minute before next play starts
         if play_delta > 1:
             show_comp_img = False
+            # if 2 seconds has passed, start next prediction and hide play result
             if play_delta > 2:
                 make_prediction = True
                 display_play_result = False
                 play_delta = 0
 
+        # display if player has won or not
         if display_play_result:
             display_result(frame, play_result, pred_cls,
                            comp_choice,
@@ -141,7 +155,7 @@ if play:
         key = cv2.waitKey(1)
 
         if show_comp_img:
-            overlay_image(frame, comp_img,
+            overlay_image(frame, comp_image,
                           (COMP_X1, COMP_Y1, COMP_X2, COMP_Y2),
                           (X_OFFSET, Y_OFFSET))
 
@@ -150,5 +164,14 @@ if play:
             break
 
         cv2.imshow("Image", frame)
+
+        if player_score == 5 or comp_score == 5:
+            break
+##################################################################################################################################
+        while True:
+            ret, frame = video.read()
+            frame = cv2.flip(frame, 1)
+
+            cv2.imshow('Image', frame)
 
 video.release()
